9b40b1f25d9ef2f7031f3b03dbb5d4e695f33bea
heopill <heopill@naver.com>
2025-05-09 15:29:46 +0900

    [FEAT] PosterCell 구현

--- NetflixClone/View/PosterCell.swift
+++ NetflixClone/View/PosterCell.swift
@@ -5,4 +5,48 @@
 //  Created by 허성필 on 5/9/25.
 //
 
-import Foundation
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    static let id = "PosterCell"
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .darkGray
+        imageView.layer.cornerRadius = 10
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+        imageView.frame = contentView.bounds
+    }
+    
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with movie: Movie) {
+        guard let posterPath = movie.posterPath else { return }
+        let urlString = "https://image.tmdb.org/t/p/w500/\(posterPath).jpg"
+        guard let url = URL(string: urlString) else { return }
+        
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.imageView.image = image
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    
+    
+}
